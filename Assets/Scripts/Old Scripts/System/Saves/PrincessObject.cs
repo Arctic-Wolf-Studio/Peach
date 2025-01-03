84eb12be4aef641043f7d5c4bcb8675cf5180899
SmulWalf <chrlcousins@gmail.com>
2025-01-03 10:03:09 -0600

    Upgrade Menu
    finished with a more optimized way of opening/closing menus

--- Assets/Scripts/Old Scripts/System/Saves/PrincessObject.cs
+++ Assets/Scripts/Old Scripts/System/Saves/PrincessObject.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 [CreateAssetMenu(menuName = "Princess", fileName = "New Princess Save Data", order = 0)]
 public class PrincessObject : ScriptableObject {
-    public int coins { get; set; }
-    public int Crowns { get; set; }
-    public int Score { get; set; }
-    public int Distance { get; set; }
-    public int CurrentBandolier { get; set; }
-    public int CurrentDress { get; set; }
-    public int CurrentWeapon { get; set;}
+    public int coins;
+    public int Crowns;
+    public int Score;
+    public int Distance;
+    public int CurrentBandolier;
+    public int CurrentDress;
+    public int CurrentWeapon;
 }
---
+++ Assets/Scripts/System/UIController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+    public static UIController instance;
+
+    [SerializeField] GameObject menu;
+    //[SerializeField] GameObject controls;
+    [SerializeField] GameObject[] windows;
+    [SerializeField] GameObject[] upgrade_menus;
+
+    private void Awake() {
+
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+    }
+
+    /*Controll the in game ui...
+      - Open and Close the menus
+      - Open back up at the landing menu
+     
+     */
+
+    public void ToggleWindow(int windowNumber) {
+        for (int i = 0; i < windows.Length; i++) {
+            if (i == windowNumber) {
+                windows[i].SetActive(!windows[i].activeInHierarchy);
+            } else {
+                windows[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ToggleUpgradeMenus(int upgradeNumber) {
+        for (int i = 0; i < upgrade_menus.Length; i++) {
+            if (i == upgradeNumber) {
+                upgrade_menus[i].SetActive(!upgrade_menus[i].activeInHierarchy);
+            } else {
+                upgrade_menus[i].SetActive(false);
+            }
+        }
+    }
+
+    public void OpenMenu() {
+        for (int i = 0; i < windows.Length; i++) {
+            windows[i].SetActive(true);
+        }
+        menu.SetActive(false);
+
+        //LevelManager.Instance.gameMenuOpen = true;
+    }
+
+    public void CloseMenu() {
+        for (int i = 0; i < windows.Length; i++) {
+            windows[i].SetActive(false);
+        }
+        menu.SetActive(true);
+
+        //LevelManager.Instance.gameMenuOpen = false;
+    }
+}
